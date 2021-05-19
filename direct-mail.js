81027cb9a9a9085db20458d90b286e7d9c72e22c
Sergio Cabral <git@sergiocabral.com>
2021-05-18 22:20:44 -0300

    Live 18/05/2021

--- direct-mail.js
+++ direct-mail.js
@@ -1,95 +1,83 @@
-/**
- * Faz uma busca no google
- * @param {string} text Texto a pesquisar.
- * @param {number} page Número da página
- * @returns {Promise<string>} Código HMTL.
- */
- async function search(text, page = 1) {
-    const skip = (page - 1) * 10;
-    console.debug(`Pesquisando termo "${text}" na página ${page}, saltando ${skip}.`);
-    const url = `https://www.google.com/search?q=${text}&hl=pt-BR&ei=xPudYMtukdLWxA_6lZ3gCw&start=${skip}&sa=N&ved=2ahUKEwjL4I6XqsjwAhURqZUCHfpKB7wQ8NMDegQIARBN&biw=766&bih=799`;
-    console.debug(`URL preparada`, url);
+async function searchAsync(text) {
+    const url = `https://www.google.com/search?q=${text}&sxsrf=ALeKk02A8YgmBIVtBGCgSpbo3wnRx7ZmzQ%3A1621378258905&source=hp&ei=0kSkYM7pNIir5OUPgeK9EA&iflsig=AINFCbYAAAAAYKRS4iS8pteKuKfQP9U99xafc2h9Q4my&oq=&gs_lcp=Cgdnd3Mtd2l6EAMYADIHCCMQ6gIQJzIHCCMQ6gIQJzIHCCMQ6gIQJzIHCCMQ6gIQJzIHCCMQ6gIQJzIHCCMQ6gIQJzIHCCMQ6gIQJzIJCCMQ6gIQJxATMgcIIxDqAhAnMgcIIxDqAhAnUABYAGCcwQFoAXAAeACAAQCIAQCSAQCYAQCqAQdnd3Mtd2l6sAEK&sclient=gws-wiz`;
     const response = await fetch(url);
     const html = await response.text();
-    console.debug(`Conteúdo obtido com ${html.length} bytes.`);
     return html;
 }
 
-/**
- * Obtem emails aleatórios do Google
- * @param {number} total Total de emails requeridos.
- * @returns {Promise<string[]>} Emails encontrados.
- */
-async function getEmails(total = 10) {
-    const surnames = [
-        "cabral",
-        "pereira",
-        "cardoso",
-        "ferreira",
-        "osvaldo",
-        "pessanha",
-        "silva"
-    ];
-    const emailProviders = [
-        "gmail.com",
-        "hotmail.com",
-        "outlook.com",
-        "ymail.com",
-        "uol.com.br",
-        "bol.com.br",
-        "terra.com.br"
-    ];
-
-    let result = [];
-
-    const maxEmptyPageAllowed = 3;
-    let emptyPageCount = 0;
-
-    while (result.length < total && 
-           emptyPageCount < maxEmptyPageAllowed) {
-
-        const surnameRandomIndex = 
-            Math.floor(Math.random() * surnames.length);
-        const emailProviderRandomIndex = 
-            Math.floor(Math.random() * emailProviders.length);
-
-        const surname = surnames[surnameRandomIndex];
-        const emailProvider = emailProviders[emailProviderRandomIndex];
-
-        const maxRandomStartPage = 3;
-        const randomStartPage = Math.floor(Math.random() * maxRandomStartPage) + 1;
-        let pageCount = 3;        
-        for (let page = randomStartPage; pageCount > 0; pageCount--) {
-            const queryTerms = `${surname} @${emailProvider}`;
-            const html = await search(queryTerms, page);
-
-            const regexHtmlTags = /<[^>]*>/g;
-            const text = html.replace(regexHtmlTags, '');
-
-            const regexEmail = /[a-z]\w{2,}@\w{3,}\.com(\.br|)/gi;
-            const emails = text.match(regexEmail) ?? [];
-
-            emails.forEach(email => console.debug(email));
-
-            if (emails.length == 0) {
-                emptyPageCount++;
-                console.debug(`Sem resultados de email para a pesquisa "${queryTerms}" na página ${page}. EmptyPageCount = ${emptyPageCount}`);
-            }
-            if (emptyPageCount == maxEmptyPageAllowed) {
-                console.warn('Não foi possível obter mais emails.');
-                break;
-            }
-
-            result.push(...emails);
-            result = result
-                .filter((value, index, array) => 
-                    array.indexOf(value) === index);
-
-            if (result.length >= total) break;
-        }
-    }
-
-    return result.slice(0, total);
+function removeHtmlTags(html) {
+    return html.replace(/<[^>]*>/g, '');
 }
 
-//await getEmails(30);
+function extractEmails(content) {
+    return content.match(/\w{3,}@\w{3,}\.com(\.br{2}|)/g);
+}
+
+function extractPhones(content) {
+    return content.match(/((?<=\+55)\d{10,11}|\(\d{2}\)\s*(9-?\d{4}|\d{4,5})-?\d{4})/g);
+}
+
+function getSurnames() {
+    return [
+        "souza",
+        "ferreira",
+        "silva",
+        "oliveira",
+        "matos",
+        "gomes",
+    ];
+}
+
+function getEmailProviders() {
+    return [
+        "gmail.com",
+        "hotmail.com",
+        "ymail.com",
+        "bol.com.br",
+        "uol.com.br"
+    ];
+}
+
+function getRandomItem(list) {
+    const randomIndex =
+        Math.floor(Math.random() * list.length);
+    return list[randomIndex];
+}
+
+function getSurname() {
+    return getRandomItem(getSurnames());
+}
+
+function getEmailProvider() {
+    return getRandomItem(getEmailProviders());
+}
+
+async function getEmails(total = 10) {
+    const surname = getSurname();
+    const emailProvider = getEmailProvider();
+    const searchTerm = `${surname} ${emailProvider}`;
+    const html = await searchAsync(searchTerm);
+    const text = removeHtmlTags(html);
+    const emails = extractEmails(text);
+    const sanitizedEmails = emails
+        .map(email => email.toLowerCase())
+        .filter((element, index, array) =>
+            array.indexOf(element) === index)
+        .sort();
+
+    return sanitizedEmails;
+}
+
+async function getPhones(total = 10) {
+    const surname = getSurname();
+    const searchTerm = `${surname} telefone`;
+    const html = await searchAsync(searchTerm);
+    const text = removeHtmlTags(html);
+    const phones = extractPhones(text);
+    const sanitizedPhones = phones
+        .filter((element, index, array) =>
+            array.indexOf(element) === index)
+        .sort();
+
+    return sanitizedPhones;
+}
